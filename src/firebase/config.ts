621bb8da7de30793a404f32b50a1480045a61afa
CJ-Yang0225 <57297482+CJ-Yang0225@users.noreply.github.com>
2021-02-23 00:44:38 +0800

    Initialize

---
+++ src/firebase/config.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCeM7-5zkWdlv43Idml1HNPXqVQyca_kXA",
+  authDomain: "cj-facebook-clone.firebaseapp.com",
+  projectId: "cj-facebook-clone",
+  storageBucket: "cj-facebook-clone.appspot.com",
+  messagingSenderId: "572034412378",
+  appId: "1:572034412378:web:96f3066e691e6efda2cd63",
+  measurementId: "G-0R6P26K35W",
+};
